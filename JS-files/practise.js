34f3d21ee7bdabbfafe4461e440a25538039fd72
Dev-302 <gebruiker302@outlook.com>
2021-06-08 21:09:19 +0200

    Created new Practice file for JS

---
+++ JS-files/practise.js
@@ -0,0 +1,22 @@
+//This is my practising program i'm using for my homework.
+
+var carName = "Mercedes";
+var carName = "Lamborghini";
+var carName = "Porsche";
+var carName = "Audi";
+
+/*One of many JavaScript HTML methods is : getElementByID().
+The example below "finds" an HTML element (with id= "demo"), and changes the element content (innerHTML) to "Hello JavaScript":*/
+
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+document.getElementById('demo').innerHTML = 'Hello JavaScript';
+//JavaScript accepts both double and singe quotes:
+
+
+// In HTML, JavaScript code is inserted between <script> and </script> tags.
+<script>
+    document.getElementById("demo").innerHTML = "My First JavaScript";
+</script> 
+
+
+
