9fcc24600066c0894ed44885444b6eaf5a470bb9
Dev-302 <gebruiker302@outlook.com>
2021-06-12 16:17:33 +0200

    Added file taretatin.js

---
+++ JS-files/taretatin.js
@@ -0,0 +1,11 @@
+//ingredients list in grams
+let puffPastery =300
+let dessertApples =900
+let goldenCasterSugar =100
+let unsaltedButter =85
+console.log(puffPastery + dessertApples + goldenCasterSugar + unsaltedButter * 9 )//Weight of all 9 tartes combined.
+
+//total fat per serving
+puffPastery =3 * 26.6
+console.log(puffPastery + unsaltedButter / 6)
+
