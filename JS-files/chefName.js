29f14afbae8dc95edb7f2e750180e128ed81826e
Dev-302 <gebruiker302@outlook.com>
2021-06-12 15:43:48 +0200

    created chefName.js git commit -m created

---
+++ JS-files/chefName.js
@@ -0,0 +1,17 @@
+let fullName = 'John Doe'
+console.log(fullName) //John Doe
+
+fullName = 'Yujean Nakamura'
+
+console.log (fullName) //Yujean Nakamura
+
+let legalDringingAge = 18
+console.log (legalDringingAge) //18
+
+/* "let" is a way to declare variables as shown above.
+with "let" you CAN reassign a value of a variable.
+You can also use "const" to declare a variable, this is prefered.
+NOTE! with "const" you cannot reassign a value as shown above.
+Only use "let" if you want to reassign a value */
+
+
