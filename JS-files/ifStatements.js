68efadb96bf28e30382fa7248430d7f28950074b
Dev-302 <gebruiker302@outlook.com>
2021-06-12 21:40:01 +0200

    added file ifStatements.js

---
+++ JS-files/ifStatements.js
@@ -0,0 +1,24 @@
+let age = 18
+if (age >= 18) {
+    console.log("adult")
+    console.log("grown-up")
+} else if (age >= 12 && age < 18) {
+    console.log("teenager")
+} else
+    console.log("child")
+
+
+    const command = '';
+    switch (command) {
+      case 'good':
+        console.log('This is good!');
+        break;
+      case 'ok':
+        console.log('This is ok');
+        break;
+      case 'terrible':
+        console.log('This is terrible!');
+        break;
+      default:
+        console.log('This is nice.');
+    }    
