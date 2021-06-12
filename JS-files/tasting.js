0534be994dc79e24037b36154d503fc382d6805b
Dev-302 <gebruiker302@outlook.com>
2021-06-12 15:44:33 +0200

    Created tasting.js

---
+++ JS-files/tasting.js
@@ -0,0 +1,38 @@
+// tasting.js
+
+console.log('we are going to tase all the datatypes!')
+console.log(42, 23, 2) //You can console.log multipe things with one statement!
+console.log()
+// You taste the rest of them. Look at the datatypes table for the syntax
+
+console.log('Hi')           // String
+console.log(true)           // Boolean
+console.log(undefined)      // Undefined
+console.log(null)           // Null
+console.log(42)             // Number
+console.log({ username: 'ABC-123', password: 'geheim' }) // Object
+console.log([ 'JS', 'CSS', 'HTML' ]) // Array
+
+// These were all the datatype syntaxes
+
+let surName = "Doe"
+let age = 18
+
+// If you've assinged all these values, you can use them ( As often as you want)
+
+console.log( surName, age)
+console.log(age, surName)
+
+// we declare the variable dish, and assing it the string value 'Ratatouille"
+let dish = 'Ratattouille'
+
+// This will also work seperately
+
+// We declare the variable dish2
+let dish2
+
+// We assing dish2 the string value "Tocino Tortilla"
+dish2 = "Tocino Tortilla"
+
+// We console.log the variable dish which holds the string value 'Ratatouille"
+console.log(dish) // 'Ratatouille'
