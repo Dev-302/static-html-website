7480016f4f0cb6bcad6802bae9a853bff05f29e8
Dev-302 <gebruiker302@outlook.com>
2021-06-13 13:18:47 +0200

    created file sayHello.js

---
+++ JS-files/sayHello.js
@@ -0,0 +1,20 @@
+// const fullName = "John Doe"
+
+// function sayHello (fullName) {
+//     console.log("Hello, " + fullName)
+// }
+
+// declaring the function
+function greet(){
+    console.log('Hello there Stranger!')
+}
+
+// calling the function
+// syntax: functionName with a set of parenthesis -> ()
+// example of a function call:
+greet()
+greet()
+
+function sayHello (firstName) {
+    console.log("Hello," + Dave)
+}
