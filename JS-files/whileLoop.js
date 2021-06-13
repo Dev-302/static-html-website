82f25d3bc7a2873fcef0b3f1edf0ecde2f650838
Dev-302 <gebruiker302@outlook.com>
2021-06-13 13:10:16 +0200

    reversed letter order

--- JS-files/whileLoop.js
+++ JS-files/whileLoop.js
@@ -6,16 +6,32 @@
 // // NOTE: this example creates an infinite loop and might crash your computer
 // // don't run it
 
-let number = 0
-while (number <= 10) {
-    console.log(number) // 1, 2, 3 etc....
-    number = number + 1 // reassign the index variable so the while loop is not infinite.
-}
+// let number = 0
+// while (number <= 10) {
+//     console.log(number) // 1, 2, 3 etc....
+//     number = number + 1 // reassign the index variable so the while loop is not infinite.
+// }
 
-const letters = ["a", "b", "c"]
-let index = 0
+// const letters = ["a", "b", "c"]
+// let index = 0
 
-while (index < letters.length) {
-    console.log(letters[index]) // logs a the first iteration, b the second iteration.. etc..
-    index = index + 1 // reassign the index variable so the while loop is not infinite
+// while (index < letters.length) {
+//     console.log(letters[index]) // logs a the first iteration, b the second iteration.. etc..
+//     index = index + 1 // reassign the index variable so the while loop is not infinite
+// }
+
+let letters = ["a", "b", "c"]
+
+// initialization:
+// initialize index to letters.length - 1 (2 in this case)
+
+// condition:
+// index >= 0 : if index is bigger or equal to zero, keep going
+
+// final experession:
+// index-- : after the loop has run, decrease index by 1
+// index-- is the same as writing index = index - 1
+
+for (let index = letters.length - 1; index >= 0; index--) {
+    console.log(letters[index])
 }
