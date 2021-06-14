8f6bc7efd5c487d89ff3cddcf96e03572e7c9762
Dev-302 <gebruiker302@outlook.com>
2021-06-14 20:28:54 +0200

    Created Happy birthday song

---
+++ JS-files/functions.js
@@ -0,0 +1,23 @@
+function name(params) {
+    
+}
+
+function // keyword 
+happyBirthday // naam
+(
+    name
+) // parameter lijst
+{
+    happyBD();
+    happyBD();
+    console.log("Happy birthday dear "+ name)
+    happyBD();
+
+} // code block
+
+function happyBD() {
+    console.log("Happy birthday to you")
+}
+happyBirthday("Friend");
+happyBirthday("F*ckface");
+happyBirthday();
